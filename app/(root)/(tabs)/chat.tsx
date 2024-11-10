384b507c23f3698f9cf4395882c19608bda5a988
Dion Ang <dionang11@gmail.com>
2024-11-10 19:25:07 +0800

    onboarding screens

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+import "../../../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Chat() {
+  return (
+    <SafeAreaView>
+      <Text>This is s</Text>
+    </SafeAreaView>
+  );
+}
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+import "../../../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <Text>This is s</Text>
+    </SafeAreaView>
+  );
+}
